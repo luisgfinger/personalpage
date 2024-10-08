3a22a0556f55dbb95db4b28dda9fd49ed34a76df
gulis007 <luisgfinger@gmail.com>
2024-10-07 21:19:59 -0300

    tentando ajustar img public

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,5 +1,5 @@
-import Logotype from '../assets/logo-group.svg'
-import DarkLogo from '../assets/logo-dark-group.svg'
+import Logotype from '../assets/logo/logo-group.svg'
+import DarkLogo from '../assets/logo/logo-dark-group.svg'
 
 interface LogoProps{
     darkMode: boolean;
